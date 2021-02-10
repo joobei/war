934c683c05492f180a328b7ab5c0b5de61a5cd31
Nikolaos Katzakis <2638631+joobei@users.noreply.github.com>
2021-02-11 00:53:58 +0100

    first commit let's do this :)

---
+++ war.hpp
@@ -0,0 +1,23 @@
+#ifndef WAR
+#define WAR
+
+namespace war {
+enum role {
+	commander = 1,
+	officer,
+	rifleman,
+	machinegunner,
+	anti_tank,
+	assault,
+	engineer,
+	support,
+	medic,
+	tank_commander,
+	crewman,
+	spotter,
+	sniper
+} role;
+
+}
+
+#endif
